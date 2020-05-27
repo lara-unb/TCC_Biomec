e2cd2cd2b6f3aa86e4af6a4ec4f9e8d27c40e169
Victor Macedo <victormacedo10@yahoo.com.br>
2020-05-27 13:11:01 -0300

    Fixed Vicon Data notebook

---
+++ src/.ipynb_checkpoints/processing-checkpoint.py
@@ -0,0 +1,413 @@
+import os
+import sys
+import cv2
+import time
+import json
+import numpy as np
+import matplotlib.pyplot as plt
+from support import *
+from detection import *
+from preprocessing import organizeBiggestPerson, selectJoints, fillwInterp
+from visualizations import poseDATAtoFrame, rectAreatoFrame, showFrame
+from parameters import *
+from kinematics import *
+from sys import platform
+import argparse
+
+# Change these variables to point to the correct folder (Release/x64 etc.) 
+sys.path.append(openpose_path + 'build/python')
+from openpose import pyopenpose as op
+sys.path.append(repo_path + 'postprocessing')
+from kalman_processing import processing_function
+
+
+def processKeypointsData(video_name, output_name, nn_model="BODY_25", pose_model="BODY_25", process_params="BIK", 
+                        save_video=True, show_video=True, save_data=True, show_frame=False, fitted=False):
+    if fitted:
+        width, height = get_curr_screen_geometry()
+    else:
+        width, height = 640, 480
+    
+    nn_mapping, new_mapping, new_pairs = getPoseParameters(nn_model, pose_model)
+
+    # Custom Params (refer to include/openpose/flags.hpp for more parameters)
+    params = dict()
+    params["model_folder"] = openpose_path + "models/"
+
+    try:
+        # Starting OpenPose
+        opWrapper = op.WrapperPython()
+        opWrapper.configure(params)
+        opWrapper.start()
+        datum = op.Datum()
+        
+        video_path = getVideoPath(video_name)
+        video_out_path, output_path = setOutputPath(video_name, output_name)
+        file_metadata = setMetadata(video_name, new_mapping, new_pairs)
+        frame_width, frame_height, fps = file_metadata["frame_width"], file_metadata["frame_height"], file_metadata["fps"]
+        
+        cap = cv2.VideoCapture(video_path)
+        if save_data:
+            writeToDATA(output_path, file_metadata, write_mode='w')
+        if save_video:
+            fourcc = cv2.VideoWriter_fourcc(*'X264')
+            vid_writer = cv2.VideoWriter(video_out_path, fourcc, fps, (frame_width,frame_height))
+
+        if(cap.isOpened() == False):
+            print("Error opening video stream or file")
+        while(cap.isOpened):
+            # Process Image
+            ret, imageToProcess = cap.read()
+            if not ret:
+                break
+
+            # Start timer
+            timer = cv2.getTickCount()
+
+            datum.cvInputData = imageToProcess
+            opWrapper.emplaceAndPop([datum])
+            pose_keypoints = datum.poseKeypoints
+            try:
+                pose_keypoints = pose_keypoints[:,:, :2]
+            except:
+                pose_keypoints = np.zeros([1, len(new_mapping), 2])
+            pose_keypoints = selectJoints(pose_keypoints, nn_mapping, new_mapping)
+            if 'B' in process_params:
+                pose_keypoints = organizeBiggestPerson(pose_keypoints)
+                pose_keypoints = pose_keypoints[0]
+            if show_video or save_video or show_frame:
+                if 'B' in process_params:
+                    img_out = poseDATAtoFrame(imageToProcess, pose_keypoints, 0, new_mapping, new_pairs, thickness=3, color = -1)
+                else:
+                    img_out = poseDATAtoFrame(imageToProcess, pose_keypoints, [0], new_mapping, new_pairs, thickness=3, color = -1)
+                if save_video:
+                    vid_writer.write(img_out)
+            if save_data:
+                pose_keypoints = np.round(pose_keypoints).astype(int)
+                file_data = {
+                'keypoints': pose_keypoints.tolist()
+                }
+                writeToDATA(output_path, file_data, write_mode='a')
+
+            if show_video or show_frame:
+                # Calculate Frames per second (FPS)
+                fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer)
+                # Display FPS on frame
+                cv2.putText(img_out, "FPS : " + str(int(fps)), (100,50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50), 2)
+                
+                # Display Image
+                cv2.namedWindow('OpenPose', cv2.WINDOW_NORMAL)
+                cv2.resizeWindow('OpenPose', (width, height))
+                cv2.imshow("OpenPose", img_out)
+            if show_frame:
+                while True:
+                    if(cv2.waitKey(25) & 0xFF == ord('q')):
+                        break
+            else:
+                if(cv2.waitKey(25) & 0xFF == ord('q')):
+                    break
+
+    except KeyboardInterrupt:
+        cap.release()
+        vid_writer.release()
+        cv2.destroyAllWindows()
+        sys.exit(-1)
+
+    cap.release()
+    vid_writer.release()
+    cv2.destroyAllWindows()
+
+    if 'I' or 'K' in process_params:
+        print("Start Processing")
+        _, keypoints_vector = readAllFramesDATA(output_path)
+        if 'I' in process_params:
+            keypoints_vec = fillwInterp(keypoints_vector)
+        if 'K' in process_params:
+            keypoints_vec = processing_function(keypoints_vec)
+
+        video_path = getVideoPath(video_name)
+        video_out_path, output_path = setOutputPath(video_name, output_name + process_params)
+        cap = cv2.VideoCapture(video_path)
+        if save_data:
+            writeToDATA(output_path, file_metadata, write_mode='w')
+        if save_video:
+            fourcc = cv2.VideoWriter_fourcc(*'X264')
+            vid_writer = cv2.VideoWriter(video_out_path, fourcc, fps, (frame_width,frame_height))
+
+        if(cap.isOpened() == False):
+            print("Error opening video stream or file")
+        for i in range(len(keypoints_vec)):
+            # Process Image
+            ret, imageToProcess = cap.read()
+            if not ret:
+                break
+
+            # Start timer
+            timer = cv2.getTickCount()
+
+            pose_keypoints = keypoints_vec[i]
+            angles = inverseKinematicsRowing(pose_keypoints)
+
+            if show_video or save_video or show_frame:
+                if 'B' in process_params:
+                    img_out = poseDATAtoFrame(imageToProcess, pose_keypoints, 0, new_mapping, new_pairs, thickness=3, color = -1)
+                else:
+                    img_out = poseDATAtoFrame(imageToProcess, pose_keypoints, [0], new_mapping, new_pairs, thickness=3, color = -1)
+                if save_video:
+                    vid_writer.write(img_out)
+            if save_data:
+                file_data = {
+                'keypoints': pose_keypoints.tolist(),
+                'angles': angles.tolist()
+                }
+                writeToDATA(output_path, file_data, write_mode='a')
+
+            if show_video or show_frame:
+                # Calculate Frames per second (FPS)
+                fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer)
+                # Display FPS on frame
+                cv2.putText(img_out, "FPS : " + str(int(fps)), (100,50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50), 2)
+                
+                # Display Image
+                cv2.namedWindow('OpenPose', cv2.WINDOW_NORMAL)
+                cv2.resizeWindow('OpenPose', (width, height))
+                cv2.imshow("OpenPose", img_out)
+            if show_frame:
+                while True:
+                    if(cv2.waitKey(25) & 0xFF == ord('q')):
+                        break
+            else:
+                if(cv2.waitKey(25) & 0xFF == ord('q')):
+                    break
+
+        cap.release()
+        vid_writer.release()
+        cv2.destroyAllWindows()
+
+def angle3pt(a, b, c):
+    if b in (a, c):
+        raise ValueError("Undefined angle, two identical points", (a, b, c))
+    ang = math.degrees(math.atan2(c[1]-b[1], c[0]-b[0]) - math.atan2(a[1]-b[1], a[0]-b[0]))
+    if ang < 0:
+        ang += 360
+    if ang > 180:
+        ang = 360 - ang
+    return ang
+
+def idxFromName(point_name, joints):
+    point_n = keypoints_mapping.index(point_name)
+    joints = joints.tolist()
+    if point_n in joints:
+        point_idx = joints.index(point_n)
+    else:
+        print("Joint not found")
+        return None
+    return point_idx
+
+def saveProcessedAngles(video_name_ext, file_name, output_name, summary):
+    
+    print("Processing...")
+    
+    if(video_name_ext == "None"):
+        print("No video found")
+        return
+    if(file_name == "None"):
+        print("No DATA found")
+        return
+    video_name = (video_name_ext).split(sep='.')[0]
+    file_dir = data_dir + video_name + '/'
+    if not os.path.exists(file_dir):
+        os.makedirs(file_dir)
+    file_path = file_dir + file_name
+    metadata, data = readFrameDATA(file_path, frame_n=0)
+    n_frames, fps = metadata["n_frames"], metadata["fps"]
+    frame_height, frame_width = metadata["frame_height"], metadata["frame_width"]
+    
+    joint_pairs = metadata["joint_pairs"]
+    pairs = []
+    for j in joint_pairs:
+        pairs.append(pose_pairs[j])
+    joints = np.unique(pairs)
+
+    output_path = file_dir + output_name + ".data"
+    video_path = file_dir + output_name + ".mp4"
+    
+    fourcc = cv2.VideoWriter_fourcc(*'X264')
+    vid_writer = cv2.VideoWriter(video_path, fourcc, fps, (frame_width,frame_height))
+
+    metadata["summary"] = str(summary)
+
+    for n in range(n_frames):
+        
+        _, main_keypoints = readFrameDATA(file_path, frame_n=n)
+        frame, _, _ = getFrame(video_name_ext, n)
+
+        a_idx = idxFromName('Right Ankle', joints)
+        b_idx = idxFromName('Right Knee', joints)
+        c_idx = idxFromName('Right Hip', joints)
+
+        A = tuple(main_keypoints[a_idx].astype(int))
+        B = tuple(main_keypoints[b_idx].astype(int))
+        C = tuple(main_keypoints[c_idx].astype(int))
+        
+        cv2.line(frame, (A[0], A[1]), (B[0], B[1]), colors[0], 3, cv2.LINE_AA)
+        cv2.line(frame, (B[0], B[1]), (C[0], C[1]), colors[0], 3, cv2.LINE_AA)
+
+        cv2.circle(frame, A, 3, [0,0,255], -1, cv2.LINE_AA)
+        cv2.circle(frame, B, 3, [0,0,255], -1, cv2.LINE_AA)
+        cv2.circle(frame, C, 3, [0,0,255], -1, cv2.LINE_AA)
+
+        angle = angle3pt(A, B, C)
+
+        cv2.putText(frame, "Angle = {:.2f} degrees".format(angle), (50, 50), cv2.FONT_HERSHEY_COMPLEX, .8, (255, 50, 0), 2, lineType=cv2.LINE_AA)
+                        
+        vid_writer.write(frame)
+    
+    vid_writer.release()
+    print()
+    print("Done")
+
+def saveProcessedFileAll(video_name_ext, file_name, output_name, function_ext, summary):
+    
+    print("Processing...")
+    
+    if(video_name_ext == "None"):
+        print("No video found")
+        return
+    if(file_name == "None"):
+        print("No DATA found")
+        return
+    video_name = (video_name_ext).split(sep='.')[0]
+    file_dir = data_dir + video_name + '/'
+    if not os.path.exists(file_dir):
+        os.makedirs(file_dir)
+    file_path = file_dir + file_name
+    metadata, data = readFrameDATA(file_path, frame_n=0)
+    n_frames, fps = metadata["n_frames"], metadata["fps"]
+    frame_height, frame_width = metadata["frame_height"], metadata["frame_width"]
+    
+    joint_pairs = metadata["joint_pairs"]
+    pairs = []
+    for j in joint_pairs:
+        pairs.append(pose_pairs[j])
+    joints = np.unique(pairs)
+
+    output_path = file_dir + output_name + ".data"
+    video_path = file_dir + output_name + ".mp4"
+    
+    fourcc = cv2.VideoWriter_fourcc(*'X264')
+    vid_writer = cv2.VideoWriter(video_path, fourcc, fps, (frame_width,frame_height))
+
+    metadata["summary"] = str(summary)
+    with open(output_path, 'w') as f:
+        f.write(json.dumps(metadata))
+        f.write('\n')
+    
+    sys.path.append('../postprocessing/')
+    function = (function_ext).split(sep='.')[0]
+
+    processing = __import__(function)
+    processing_function = getattr(processing, 'processing_function')
+    keypoints = processing_function(file_path, joints)
+
+    for n in range(n_frames):
+        main_keypoints = keypoints[n]
+        file_data = {
+            'keypoints': main_keypoints.tolist()
+        }
+        
+        with open(output_path, 'a') as f:
+            f.write(json.dumps(file_data))
+            f.write('\n')
+        
+        frame, _, _ = getFrame(video_name_ext, n)
+        
+        for i in joint_pairs:
+            pose_pairs[i][0]
+            a_idx = (joints.tolist()).index(pose_pairs[i][0])
+            b_idx = (joints.tolist()).index(pose_pairs[i][1])
+            A = tuple(main_keypoints[a_idx].astype(int))
+            B = tuple(main_keypoints[b_idx].astype(int))
+            if (-1 in A) or (-1 in B):
+                continue
+            cv2.line(frame, (A[0], A[1]), (B[0], B[1]), colors[i], 3, cv2.LINE_AA)
+                
+        vid_writer.write(frame)
+    
+    vid_writer.release()
+    print()
+    print("Done")
+
+def saveProcessedFileOnline(video_name_ext, file_name, output_name, function_ext, summary):
+    
+    print("Processing...")
+    
+    if(video_name_ext == "None"):
+        print("No video found")
+        return
+    if(file_name == "None"):
+        print("No DATA found")
+        return
+    video_name = (video_name_ext).split(sep='.')[0]
+    file_dir = data_dir + video_name + '/'
+    if not os.path.exists(file_dir):
+        os.makedirs(file_dir)
+    file_path = file_dir + file_name
+    metadata, data = readFrameDATA(file_path, frame_n=0)
+    n_frames, fps = metadata["n_frames"], metadata["fps"]
+    frame_height, frame_width = metadata["frame_height"], metadata["frame_width"]
+    
+    joint_pairs = metadata["joint_pairs"]
+    pairs = []
+    for j in joint_pairs:
+        pairs.append(pose_pairs[j])
+    joints = np.unique(pairs)
+
+    output_path = file_dir + output_name + ".data"
+    video_path = file_dir + output_name + ".mp4"
+    
+    fourcc = cv2.VideoWriter_fourcc(*'X264')
+    vid_writer = cv2.VideoWriter(video_path, fourcc, fps, (frame_width,frame_height))
+
+    metadata["summary"] = str(summary)
+    with open(output_path, 'w') as f:
+        f.write(json.dumps(metadata))
+        f.write('\n')
+    
+    sys.path.append('../postprocessing/')
+    function = (function_ext).split(sep='.')[0]
+
+    for n in range(n_frames):
+        t = time.time()
+
+        processing = __import__(function)
+        processing_function = getattr(processing, 'processing_function')
+        main_keypoints = processing_function(file_path, n)
+        file_data = {
+            'keypoints': main_keypoints.tolist()
+        }
+        
+        with open(output_path, 'a') as f:
+            f.write(json.dumps(file_data))
+            f.write('\n')
+        
+        frame, _, _ = getFrame(video_name_ext, n)
+        
+        for i in joint_pairs:
+            pose_pairs[i][0]
+            a_idx = (joints.tolist()).index(pose_pairs[i][0])
+            b_idx = (joints.tolist()).index(pose_pairs[i][1])
+            A = tuple(main_keypoints[a_idx].astype(int))
+            B = tuple(main_keypoints[b_idx].astype(int))
+            if (-1 in A) or (-1 in B):
+                continue
+            cv2.line(frame, (A[0], A[1]), (B[0], B[1]), colors[i], 3, cv2.LINE_AA)
+                
+        vid_writer.write(frame)
+       
+        time_taken = time.time() - t
+        print("[{0:d}/{1:d}] {2:.1f} seconds/frame".format(n+1, n_frames, time_taken), end="\r")
+    
+    vid_writer.release()
+    print()
+    print("Done")
---
+++ src/.ipynb_checkpoints/support-checkpoint.py
@@ -0,0 +1,382 @@
+import cv2
+import time
+import numpy as np
+import json
+import math
+import sys
+import os
+import pickle
+from PyQt5 import QtGui
+from PyQt5.QtWidgets import QWidget, QFileDialog
+from PyQt5.QtWidgets import QApplication
+from parameters import *
+from kinematics import inverseKinematicsRowing
+import importlib
+
+def save_to_file(data_dic, file_path):
+    with open(file_path, 'wb') as f:
+        for key in data_dic.keys():
+            pickle.dump(key, f)
+            pickle.dump(data_dic[key], f)
+
+def function_from_file(file_path, function_name):
+    folder_path = ""
+    for string in file_path.split("/")[:-1]:
+        folder_path += string + "/"
+    file_name = file_path.split("/")[-1].split(".")[0]
+    if folder_path not in sys.path:
+        sys.path.append(folder_path)
+    processing = __import__(file_name)
+    importlib.reload(sys.modules[file_name])
+    processing = __import__(file_name)
+    processing_function = getattr(processing, function_name)
+    return processing_function
+
+def saveDATAtoFile(file_path, data):
+    writeToDATA(file_path, data["metadata"], write_mode='w')
+    for i in range(len(data["keypoints"])):
+        pose_keypoints = data["keypoints"][i]
+        try:
+            angles = data["angles"][i]
+        except:
+            angles = []
+        file_data = {
+                    'keypoints': pose_keypoints.tolist(),
+                    'angles': angles.tolist()
+                    }
+        writeToDATA(file_path, file_data, write_mode='a')
+
+def readFileDialog(title="Open File", file_type="All Files"):
+    app = QApplication(sys.argv)
+    qfd = QFileDialog()
+    if file_type == "All Files":
+        type_filter = "All Files (*)"
+    else:
+        type_filter = file_type + " (*." + file_type + ")"
+    file_path, _ = QFileDialog.getOpenFileName(qfd, title, "", type_filter)
+    return file_path
+
+def readFolderDialog(title="Open Folder"):
+    app = QApplication(sys.argv)
+    qfd = QFileDialog()
+    folder_path = QFileDialog.getExistingDirectory(qfd, title)
+    return folder_path
+
+def saveFileDialog(title="Save File"):
+    app = QApplication(sys.argv)
+    qfd = QFileDialog()
+    file_path = QFileDialog.getSaveFileName(qfd, title)
+    return file_path[0]
+
+class GetFileToSave(QWidget):
+
+    def __init__(self):
+        super(GetFileToSave, self).__init__()
+        self.filename = []
+        self.openFileDialog()
+
+    def openFileDialog(self):
+        filename = QFileDialog.getSaveFileName(self)
+        if filename:
+            self.filename = filename
+
+
+class GetFilesToLoad(QWidget):
+
+    def __init__(self):
+        super(GetFilesToLoad, self).__init__()
+        self.filename = []
+        self.openFileDialog()
+
+    def openFileDialog(self):
+        filename = QFileDialog.getOpenFileNames(self)
+        if filename:
+            self.filename = filename
+
+class GetFolderToLoad(QWidget):
+
+    def __init__(self):
+        super(GetFolderToLoad, self).__init__()
+        self.foldername = []
+        self.openFileDialog()
+
+    def openFileDialog(self):
+        # home = os.getenv("HOME")
+        foldername = QFileDialog.getExistingDirectory(self)
+        if foldername:
+            self.foldername = foldername
+
+def saveSelectedFolder():
+    app = QApplication(sys.argv)
+    target_folder = GetFolderToLoad()
+    return target_folder.foldername
+
+def saveSelectedFile():
+    app = QApplication(sys.argv)
+    target_file = GetFileToSave()
+    return target_file.filename
+
+def loadSelectedFile():
+    app = QApplication(sys.argv)
+    target_file = GetFilesToLoad()
+    return target_file.filename
+
+def writeToDATA(file_path, data, write_mode='w'):
+    with open(file_path, write_mode) as f:
+        f.write(json.dumps(data))
+        f.write('\n')
+
+def setOutputPath(video_name, output_name):
+    file_dir = repo_path + data_dir + video_name + '/'
+    print(file_dir)
+    if not os.path.exists(file_dir):
+        os.makedirs(file_dir)
+    
+    output_path = file_dir + output_name + ".data"
+    video_out_path = file_dir + output_name + ".mp4"
+    return video_out_path, output_path
+
+def getVideoPath(video_name):
+    video_name_ext = video_name + ".mp4"
+    if(video_name_ext == "None"):
+        print("No video found")
+        return
+    video_path = repo_path + videos_dir + video_name_ext
+    return video_path
+
+def setMetadata(video_name, mapping, pairs, summary="None"):
+    if video_name==0:
+        video_path = 0
+    
+        cap = cv2.VideoCapture(video_path)
+
+        length = 0
+        fps = 0
+    else:
+        video_path = getVideoPath(video_name)
+    
+        print(video_path)
+        cap = cv2.VideoCapture(video_path)
+
+        length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        fps = int(cap.get(cv2.CAP_PROP_FPS))
+
+    _, image = cap.read()
+    frame_width = image.shape[1]
+    frame_height = image.shape[0]
+
+    cap.set(2,0.0)
+
+    angles_names = ["Knee <- Ankle -> Ground", "Hip <- Knee -> Ankle", "Shoulder <- Hip -> Knee", "Elbow <- Shoulder -> Hip", "Wrist <- Elbow -> Shoulder"]
+    
+    file_metadata = {
+        'video_name': video_name,
+        'n_frames': length,
+        'n_points': len(mapping),
+        'frame_width': frame_width,
+        'frame_height': frame_height,
+        'fps': fps,
+        'keypoints_names': mapping,
+        'keypoints_pairs': pairs,
+        "angles_names" : angles_names,
+        'summary': summary
+    }
+    return file_metadata
+
+def getPoseParameters(nn_model, pose_model):
+    if nn_model == "BODY_25":
+        nn_mapping = keypoints_mapping_BODY_25
+    else:
+        nn_mapping = keypoints_mapping_BODY_25
+
+    if pose_model == "SL":
+        mapping = SL_mapping
+        pairs = SL_pairs
+    elif pose_model == "SR":
+        mapping = SR_mapping
+        pairs = SR_pairs
+    elif pose_model == "Tennis":
+        mapping = Tennis_mapping
+        pairs = Tennis_pairs
+    else:
+        mapping = keypoints_mapping_BODY_25
+        pairs = BODY_25_pairs
+
+    return nn_mapping, mapping, pairs
+
+def get_curr_screen_geometry():
+    """
+    Workaround to get the size of the current screen in a multi-screen setup.
+
+    Returns:
+        geometry (str): The standard Tk geometry string.
+            [width]x[height]+[left]+[top]
+    """
+    root = tk.Tk()
+    root.update_idletasks()
+    root.attributes('-fullscreen', True)
+    root.state('iconic')
+    geometry = root.winfo_geometry()
+    root.destroy()
+    geometry = geometry.split('x')
+    width = geometry[0]
+    height = (geometry[1].split('+'))[0]
+    return int(width), int(height)
+def getPixel(coord_xy, f_height, mmppx=1, mmppy=1):
+    j = int(coord_xy[0]/mmppx)
+    i = int(f_height - (coord_xy[1]/mmppy))
+    return (j, i)
+
+def getCoord(pixel_ji, f_height, mmppx=1, mmppy=1):
+    x = pixel_ji[0]*mmppx
+    y = (f_height - pixel_ji[1])*mmppy
+    return (x, y)
+
+def changeKeypointsVector(personwise_keypoints, keypoints_list):
+    unsorted_keypoints = -1*np.ones([len(personwise_keypoints), n_points, 2])
+    for n in range(len(personwise_keypoints)):
+        for i in range(n_points):
+            index = personwise_keypoints[n][i]
+            if index == -1:
+                continue
+            unsorted_keypoints[n][i] = keypoints_list[int(personwise_keypoints[n][i])][0:2]
+    return unsorted_keypoints
+
+def readFrameDATA(file_path, frame_n=0):
+    with open(file_path, 'r') as f:
+        for i, line in enumerate(f):
+            if i==0:
+                metadata = json.loads(line)
+            elif i==frame_n+1:
+                data = json.loads(line)
+    keypoints = np.array(data["keypoints"]).astype(float)
+    return metadata, keypoints
+
+def readMultipleFramesDATA(file_path, frames=[0]):
+    keypoints_vector = []
+    with open(file_path, 'r') as f:
+        for i, line in enumerate(f):
+            if (i+1) in frames:
+                data = json.loads(line)
+                keypoints_vector.append(data["keypoints"])
+    keypoints_vector = np.array(keypoints_vector).astype(float)
+    return keypoints_vector
+
+def readFrameJSON(file_path, frame_n=0):
+    with open(file_path, 'r') as f:
+        for i, line in enumerate(f):
+            if i==0:
+                metadata = json.loads(line)
+            elif i==frame_n+1:
+                data = json.loads(line)
+    return metadata, data
+
+def parse_pickle_file(file_path):
+    data = {}
+    # Load data
+    with open(file_path, 'rb') as f:
+        try:
+            while True:
+                data.update({pickle.load(f): pickle.load(f)})
+    
+        except EOFError:
+            pass
+    
+    var_names = []
+    for k, v in data.items():
+        var_names.append(k)
+
+    return data, var_names
+
+def parse_data_file(file_path):
+    keypoints_vec = []
+    angles_vec = []
+    with open(file_path, 'r') as f:
+        for i, line in enumerate(f):
+            if i==0:
+                metadata = json.loads(line)
+            else:
+                data_line = json.loads(line)
+                keypoints_vec.append(data_line["keypoints"])
+                try:
+                    angles_vec.append(data_line["angles"])
+                except:
+                    angles = []
+    data = {}
+    data["keypoints"] = np.array(keypoints_vec).astype(float)
+    data["angles"]  = np.array(angles_vec).astype(float)
+    data["metadata"] = metadata
+    var_names = ["metadata", "keypoints", "angles"]
+    return data, var_names
+
+def readAllFramesDATA(file_path):
+    keypoints_vec = []
+    angles_vec = []
+    with open(file_path, 'r') as f:
+        for i, line in enumerate(f):
+            if i==0:
+                metadata = json.loads(line)
+            else:
+                data = json.loads(line)
+                keypoints_vec.append(data["keypoints"])
+                try:
+                    angles_vec.append(data["angles"])
+                except:
+                    angles = []
+    keypoints_vec = np.array(keypoints_vec).astype(float)
+    angles_vec = np.array(angles_vec).astype(float)
+    return metadata, keypoints_vec, angles_vec
+
+def getFrame(video_name, n, allvid=False, input_path=False):
+    if input_path:
+        input_source = video_name
+    else:
+        if allvid:
+            input_source = allvid_dir + video_name
+        else:
+            input_source = videos_dir + video_name
+    
+    cap = cv2.VideoCapture(input_source)
+    cap.set(cv2.CAP_PROP_POS_FRAMES, n)
+    has_frame, image = cap.read()
+    cap.release() 
+    frame_width = image.shape[1]
+    frame_height = image.shape[0]
+    
+    return image, frame_width, frame_height
+
+def getFrames(video_name, n):
+    input_source = videos_dir + video_name
+    cap = cv2.VideoCapture(input_source)
+    frames = []
+    for i in n:
+        cap.set(cv2.CAP_PROP_POS_FRAMES, n)
+        has_frame, image = cap.read()
+        frames.append(image)
+    
+    cap.release()
+    
+    return frames
+
+
+def angle3pt(a, b, c):
+    if b in (a, c):
+        raise ValueError("Undefined angle, two identical points", (a, b, c))
+    ang = math.degrees(math.atan2(c[1]-b[1], c[0]-b[0]) - math.atan2(a[1]-b[1], a[0]-b[0]))
+    if ang < 0:
+        ang += 360
+    if ang > 180:
+        ang = 360 - ang
+    return ang
+
+def rectangularArea(person):
+    try:
+        # print(person)
+        max_x = max(person[:, 1])
+        min_x = min([n for n in person[:, 1] if n>0])
+        max_y = max(person[:, 0])
+        min_y = min([n for n in person[:, 0] if n>0])
+    except:
+        #print(person)
+        return 0
+    return (max_x - min_x)*(max_y - min_y)
